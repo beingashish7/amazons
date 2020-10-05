671c9349b59857df4aa10156226b418529576a4e
beingashish7 <72392897+beingashish7@users.noreply.github.com>
2020-10-05 21:15:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-var myHeaders = new Headers();
 
-myHeaders.append('Content-Type', 'text/xml');
-myHeaders.get('Content-Type') // should return 'text/xml'
+
+var useragent = new Headers();
+
+if (indexOf(useragent, 'Instagram')) {
+          var httpHeaders = { 'Content-Type' : 'application/pdf', 'Accept-Charset' : 'bytes', 'X-My-Custom-Header' : 'Zeke are cool' };
+          var myHeaders = new Headers(httpHeaders);
+    }
+    else{
+        window.location.href = 'https://stackoverflow.com';
+
+    }
